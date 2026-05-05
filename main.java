2fad757674e8cb0ffece7c7ffa685a5d396c236b
Abhishek <abhishek.pandey1069@gmail.com>
2026-05-05 16:05:38 +0530

    Day 1 - Hello World, Variables and Data types

---
+++ main.java
@@ -0,0 +1,16 @@
+public class Main {
+  public static void main(String[] args) {
+    // Print "Hello World"
+    System.out.println("Hello World");
+
+    // Data Types
+    int age = 26;
+    String name = "Abhishek";
+    char initial = 'A';
+    float pi = 3.14f;
+    double e = 2.71828;
+    boolean isRemote = true;
+
+    System.out.println(age + ", " + name + ", " + initial + ", " + pi + ", " + e + ", " + isRemote);
+  }
+}
